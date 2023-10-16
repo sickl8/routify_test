dcb6742eef76a9c920723832d14ec9e4c9be62a5
Ilyass Saadi <isaadi@e1r7p6.1337.ma>
2023-10-16 10:41:25 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+    import { InternalReflector, Router, createRouter } from '@roxi/routify';
+    import routes from '../.routify/routes.default.js';
+    import './app.css';
+
+	export const router = createRouter({ routes });
+	const urlRewrite = {
+		toInternal: (url: string) => {
+			console.log({url})
+			return `#${url}`;
+		},
+		toExternal: (url: string) => {
+			console.log({url})
+			return url.replace(/^.*#/, "");
+		}
+	}
+</script>
+
+<Router {router} {urlRewrite} urlReflector={InternalReflector}/>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let name = 'Routify 3';
+</script>
+
+<div class="container mx-auto p-10">
+    <h1 class="text-center text-4xl text-red-600 bg-red-100 p-2 rounded-full">{name}</h1>
+</div>
